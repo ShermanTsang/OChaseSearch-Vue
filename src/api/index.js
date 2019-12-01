78b54238b5ffdc52d0861c56a060decdb95c5db0
ShareManT <sharemant@163.com>
2019-12-01 23:24:34 +0800

    chore: update axios config(test)

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,16 @@
 import Axios from 'axios'
 
+Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded'
+Axios.defaults.transformRequest = [data=> {
+    let ret = ''
+    for (let it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+    }
+    return ret
+}]
+Axios.defaults.withCredentials = true
+
 Axios.interceptors.response.use(res => {
     console.log(`[ Response: ${res.config.url} ]`)
     return res.data
